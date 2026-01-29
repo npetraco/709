8d52601eec5bb9b339ae32e7679dd8c4cc44653d
npetraco <npetraco@gmail.com>
2026-01-28 21:04:22 -0500

    update

---
+++ R/script_bucket/dice6.R
@@ -0,0 +1,31 @@
+# Sample from 1 through 6, 1000 times:
+n    <- 1000
+samp <- sample(c(1,2,3,4,5,6), size=n, replace=T)
+samp
+
+sum(samp==3)/n # Frequency of rolling a 3 in 1000 rolls
+
+
+n          <- 1000
+num.trials <- 100
+dsim       <- array(-1, c(n, num.trials))
+for(i in 1:n){
+
+  for(j in 1:num.trials){
+    samp <- sample(c(1,2,3,4,5,6), size=i, replace=T)
+
+    freq3 <- sum(samp==3)/i# Frequency of rolling a 3 in 1000 rolls
+    #print(freq3)
+    dsim[i,j] <- freq3
+  }
+
+}
+
+#Plot
+plot(rep(1,num.trials), dsim[1,], xlim=c(1,n), ylim=c(0,1), pch=16, col="blue", xlab="Number of Rolls", ylab="Frequency")
+for(i in 2:n){
+  points(rep(i,num.trials), dsim[i,], pch=16, col="blue")
+}
+abline(a = 1/6, b = 0)
+
+
